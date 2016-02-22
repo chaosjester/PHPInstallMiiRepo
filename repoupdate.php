47f574ae132385630a9a26dc6e23f03ba1b40c53
chaosjester <fb@chaosjester.net>
2016-02-22 14:11:13 +1100

    SMDH file scraping now working
    Have managed to pull package name, description and author from the SMDH
    file.  Now to make it throw it in to the packages.json file!

--- repoupdate.php
+++ repoupdate.php
@@ -9,6 +9,17 @@ file_put_contents($reporoot.'/repo.list', $repofile);
 // Create Shell Script to create package.list
 $packagefile = 'for d in '.$repodir.'/*; do cd ${d} && rm -f package.* && find . -type f | sed s,^./,, > package.tmp && sed "/package.tmp/d" package.tmp > package.list && rm package.tmp  && cd ..; done';
 file_put_contents($repodir.'/packagelistgen.sh', $packagefile);
+
+// Create SMDH Scraper
+$scrapefile = 'for f in $(find '.$repodir.' -name "*.smdh"); do dd if=$f ibs=1 skip=08 count=80 > $f.txt && printf "\n" >> $f.txt && dd if=$f ibs=1 skip=136 count=200 >> $f.txt && printf "\n" >> $f.txt && dd if=$f ibs=1 skip=392 count=80 >> $f.txt && printf "\n" >> $f.txt && tr -d "\0" < $f.txt >> '.$repodir.'/master.list && rm -f $f.txt; done';
+file_put_contents($repodir.'/scrapesmdh.sh', $scrapefile);
+
+// Run shell script to create package.list and scrape SMDH Files
+shell_exec('chmod +x '.$repodir.'/packagelistgen.sh');
+shell_exec('chmod +x '.$repodir.'/scrapesmdh.sh');
+shell_exec($repodir.'/packagelistgen.sh');
+shell_exec($repodir.'/scrapesmdh.sh');
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,14 +35,6 @@ file_put_contents($repodir.'/packagelistgen.sh', $packagefile);
 </head>
 <body>
 
-<?php
-// Create shell script to create package.list
-//shell_exec('touch '.$repodir.'/pacakgelist.sh');
-//shell_exec('chmod +x '.$repodir.'/pacakgelist.sh');
-shell_exec('chmod +x '.$repodir.'/packagelistgen.sh');
-shell_exec($repodir.'/packagelistgen.sh');
-echo "<pre>$output</pre>";
-?>
 
   <!--Import jQuery before materialize.js-->
   <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
