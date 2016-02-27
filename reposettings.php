8074eccf009fc714ada646fd17691e0f8e0ce5b2
LiquidFenrir <tatoune77021@hotmail.fr>
2016-02-27 11:16:58 +0100

    removing settings no longer needed

--- reposettings.php
+++ reposettings.php
@@ -5,13 +5,4 @@ $repourl = "http://yourrepoaddress.com/";
 $repoowner = "Your Name";
 $repoblurb = "This text will be displayed on the footer of the index page";
 
-// File Path settings
-// ABSOLUTE PATH REQUIRED
-// $reporoot example /var/www/repo
-// $repodir example /var/www/repo/3ds
-
-$reporoot = realpath('/foo/bar');
-$repodir = realpath('/foo/bar/3ds');
-
-
 ?>
