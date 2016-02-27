681947ff591f6c6885de509465b8b4508cb62377
LiquidFenrir <tatoune77021@hotmail.fr>
2016-02-27 04:48:02 +0100

    Added files via upload
    scraping the data from the .smdh files in php instead of sh

--- repoupdate.php
+++ repoupdate.php
@@ -7,26 +7,12 @@ $repofile = '{"repos":[{"name":"'.$reponame.'","url":"'.$repourl.'"}]}';
 file_put_contents($reporoot.'/repo.list', $repofile);
 
 // Create Shell Script to create package.list
-$packagefile = 'for d in '.$repodir.'/*; do cd ${d} && rm -f package.* && rm -f *.smdh.txt && find . -type f | sed s,^./,, > package.tmp && sed "/package.tmp/d" package.tmp > package.list && rm package.tmp  && cd ..; done';
+$packagefile = 'for d in '.$repodir.'/*; do cd ${d} && rm -f package.* && find . -type f | sed s,^./,, > package.tmp && sed "/package.tmp/d" package.tmp > package.list && rm package.tmp  && cd ..; done';
 file_put_contents($repodir.'/packagelistgen.sh', $packagefile);
 
-// Create SMDH Scraper
-unlink('master.list');
-$scrapefile = 'for f in $(find '.$repodir.' -name "*.smdh"); do printf "name = " > $f.txt && dd if=$f ibs=1 skip=08 count=80 >> $f.txt && printf "\nshort_description = " >> $f.txt && dd if=$f ibs=1 skip=136 count=200 >> $f.txt && printf "\nauthor = " >> $f.txt && dd if=$f ibs=1 skip=392 count=80 >> $f.txt && path=$f && printf "\ncategory = nul\nwebsite = nul\ntype = 3ds\nversion = nul\n" >> $f.txt && echo "dl_path = nul\ninfo_path = ${path#'.$reporoot.'}" >> $f.txt && tr -d "\0" < $f.txt >> '.$reporoot.'/master.list && rm -f $f.txt; done';
-file_put_contents($repodir.'/scrapesmdh.sh', $scrapefile);
-
-// Run shell script to create package.list and scrape SMDH Files
+// Run shell script to create package.list
 shell_exec('chmod +x '.$repodir.'/packagelistgen.sh');
-shell_exec('chmod +x '.$repodir.'/scrapesmdh.sh');
 shell_exec($repodir.'/packagelistgen.sh');
-shell_exec($repodir.'/scrapesmdh.sh');
-
-$dir = "./3ds/";
-$apps = array_diff(scandir($dir), array('..', '.', 'packagelistgen.sh', 'scrapesmdh.sh'));
-sort($apps);
-$flipped = array_flip($apps);
-$dl_path = $apps;
-foreach ($dl_path as &$item) { $item = "3ds/".$item."/"; }
 ?>
 <!DOCTYPE html>
 <html>
@@ -43,7 +29,13 @@ foreach ($dl_path as &$item) { $item = "3ds/".$item."/"; }
 <body>
 <div class="container">
 <?php
-// Big thanks to LiquidFenrir for his assistance (by assistance I mean writing all the code below!)
+$apps = array_diff(scandir($repodir), array('..', '.', 'packagelistgen.sh', 'scrapesmdh.sh')); // getting all the apps in the 3ds folder
+sort($apps);
+$dl_path = $apps;
+$info_path = $apps;
+foreach ($dl_path as &$item) { $item = "3ds/".$item."/"; }
+foreach ($info_path as &$item) { $item = "3ds/".$item."/".$item.".smdh"; }
+
 $repoInfo = array();
 $repoInfo["name"] = $reponame;
 $repoInfo["author"] = $repoowner;
@@ -60,31 +52,42 @@ $fields[] = "version";
 $fields[] = "dl_path";
 $fields[] = "info_path";
 
-$lines = file("master.list", FILE_IGNORE_NEW_LINES);
-$i = 1;
-for ($i;$i <= sizeof($apps);$i++) {
-  if (strpos($lines[$i*9-1], 'info_path = ') !== false) {
-    $id = explode("/",$lines[$i*9-1]);
-    $lines[$i*9-2] = "dl_path = ".$dl_path[$flipped[$id[2]]];
-    $lines[$i*9-1] = substr($lines[$i*9-1],1);
+$i = 0;
+for($i;$i <=sizeof($apps)-1;$i++) { // removing apps from the array if they don't have a .smdh in their folder
+  if(!file_exists("./".$info_path[$i])) {
+    unset($apps[$i]);
+    unset($info_path[$i]);
+    unset($dl_path[$i]);
   }
 }
-$list = array_chunk($lines, sizeof($fields)); // cut the array that contained the lines into pieces to separate each app
-$size = sizeof($list)-1;
+sort($apps);
+sort($dl_path);
+sort($info_path);
 
+$list = array();
 $i = 0;
-for($i;$i <= $size;$i++) { // change the name of keys in the array to make the json compatible with installMii
-  $j = 0;
-  for($j;$j <= sizeof($fields)-1;$j++) {
-    $list[$i][$fields[$j]] = substr($list[$i][$j],strlen($fields[$j])+3); // remove the tags to only have the value
-    if ($list[$i][$fields[$j]] == "nul") { $list[$i][$fields[$j]] = null;}
-    unset($list[$i][$j]); // remove the old key that is not needed anymore and only takes space (and would mess with json_encode)
-  }
+for ($i;$i <= sizeof($apps)-1;$i++) { // scrape the SMDH files
+  $list[$i] = array();
+  $file = file_get_contents($info_path[$i]);
+  $list[$i][$fields[0]] = substr($file,8,80); //reading the parts with the name, desc and author in the smdh
+  $list[$i][$fields[1]] = substr($file,392,80);
+  $list[$i][$fields[2]] = substr($file,136,100);
+  // those null parts are where the master.list idea can be better than using php to read the smdh, but I don't think there's a zone for version in a smdh
+  $list[$i][$fields[3]] = null;
+  $list[$i][$fields[4]] = null;
+  $list[$i][$fields[5]] = "3ds"; // to make installMii install in the sd:/3ds/ folder
+  $list[$i][$fields[6]] = null;
+  $list[$i][$fields[7]] = $dl_path[$i];
+  $list[$i][$fields[8]] = $info_path[$i];
 }
 
 // create a new array before encoding to get the "repo" and "packages" part correctly
 $formattedjson = json_encode(array("repo"=>$repoInfo, "packages"=>$list));
-$formattedjson = str_replace("\/","/",$formattedjson);
+// somehow using php to read the smdh pads every letter with a null byte
+// I don't know how to get rid of it in the input, but we can simply remove it in the output
+// could replace the str_replace() for escaped slashes by JSON_UNESCAPED_SLASHES in the json_encode() but requires at least php 5.4
+$formattedjson = str_replace("\u0000","",str_replace("\/","/",$formattedjson));
+
 if (!$formattedjson){
 	echo '<div class="col s12 m6 offset-m3">
           <div class="card red darken-1 center-align">
@@ -97,7 +100,7 @@ if (!$formattedjson){
 } else {
 echo ' <div class="col s12 m6 offset-m3">
           <div class="card green darken-1 center-align">
-            <div class="card-content white-text">
+            <div class="card-content white-text left-align">
               <span class="card-title">Successfully added to packages.json</span>
               <p>'.$formattedjson.'</p>
             </div>
